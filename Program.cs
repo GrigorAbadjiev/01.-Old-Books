35fb8cd5d66260f5c2cbd304b7e8ba3ef269658e
User <User@DESKTOP-ESP2JT1>
2022-12-01 17:31:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedBook = Console.ReadLine();
+            bool isBookFound = false;
+            int booksSearched = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "No More Books")
+                {
+                    break;
+                }
+                if (input == searchedBook)
+                {
+                    isBookFound = true;
+                    break;
+                }
+                booksSearched++;
+
+            }
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {booksSearched} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {booksSearched} books.");
+            }
+        }
+    }
+}
